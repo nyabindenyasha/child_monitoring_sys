29e0d16d064f40c1edc44b681f3148afedfbb5a8
nyashan@afrosoft.co.zw <nyashan@afrosoft.co.zw>
2020-06-18 23:21:07 +0200

    commit

--- src/main/java/com/minimum/ChildTrackingApplication.java
+++ src/main/java/com/minimum/ChildTrackingApplication.java
@@ -2,10 +2,10 @@ package com.minimum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@RestController
+@EnableScheduling
 public class ChildTrackingApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/minimum/ScheduledTasks.java
@@ -0,0 +1,20 @@
+package com.minimum;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ScheduledTasks {
+
+    @Scheduled(fixedRate = 60000)
+    public void scheduleTaskWithFixedRate() {
+        System.out.println();
+        final String uri = "http://www.glendoretracking.co.za/functions/hardwarecommunicator.php?regNumber=AAA-4579&long=28.540582&lat=-20.173766&fuelLevel=95&ignition=1&weight=300.34&hardware=true";
+        System.out.println("uri: " + uri);
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        System.out.println("result: " + result);
+    }
+}
+
