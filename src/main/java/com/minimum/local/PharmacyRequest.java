c82137a24fc23a8688ec1d2f7fe37c35e53aeafb
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2020-12-07 14:23:58 +0200

    commit

---
+++ src/main/java/com/minimum/local/PharmacyRequest.java
@@ -0,0 +1,41 @@
+package com.minimum.local;
+
+public class PharmacyRequest {
+
+    private String  fullName;
+    private String  email;
+    private String  phoneNumber;
+    private String  location;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
